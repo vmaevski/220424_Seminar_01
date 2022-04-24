f3dd913db7a08de1cad72379df325cc316bfba08
vmaevski <vmaevski@mail.ru>
2022-04-24 15:13:24 +0700

    Решили задачу 2.1

---
+++ 03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Итерация №2
+// 3. Напишите программу вычисления функции:
+//  x = f(a) = a^2-10
+
+
+string s_a1 = Console.ReadLine();
+int a1 = int.Parse(s_a1);
+
+Console.WriteLine(a1 * a1 -10);
